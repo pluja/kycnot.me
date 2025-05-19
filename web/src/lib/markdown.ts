2657f936bc80b711ca41003b8274ab8abebab4f1
pluja <codeberg@r3d.red>
2025-05-19 10:19:49 +0000

    Release 2025-05-19

--- web/src/lib/markdown.ts
+++
@@ -1,5 +0,0 @@
-/** A string containing Markdown. */
-export type MarkdownString = string
-
-/** A string containing HTML. */
-export type HtmlString = string
--- web/src/lib/timeTrapSecret.ts
+++
@@ -1,6 +0,0 @@
-import crypto from 'crypto'
-
-// Generate a 32-byte secret key once when the module is first loaded
-const timeTrapSecretKey = crypto.randomBytes(32)
-
-export { timeTrapSecretKey }
