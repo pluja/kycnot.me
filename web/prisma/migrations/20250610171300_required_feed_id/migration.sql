ad3c5614190071b1d0ed78b0f43c0772fe589076
pluja <codeberg@r3d.red>
2025-06-10 17:48:58 +0000

    Release 202506101748

--- web/prisma/migrations/20250610171300_required_feed_id/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `feedId` on table `User` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "User" ALTER COLUMN "feedId" SET NOT NULL;
