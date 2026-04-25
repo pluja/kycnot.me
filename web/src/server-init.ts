71b346680b67f4fbc497ea4dd8692fa964e8f733
pluja <codeberg@r3d.red>
2026-04-25 19:34:32 +0300

    fix(astro): exit server-init promptly on SIGTERM

--- web/src/server-init.ts
+++ web/src/server-init.ts
@@ -1,6 +1,12 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import { startListener } from './lib/postgresListeners'
 
+const exitOnSignal = (signal: NodeJS.Signals) => {
+  process.on(signal, () => process.exit(0))
+}
+exitOnSignal('SIGTERM')
+exitOnSignal('SIGINT')
+
 await startListener({
   error: console.error,
   warn: console.warn,
