67f546a00e8daf8c61eaac7940b2ef6db2f5e8f1
pluja <codeberg@r3d.red>
2026-05-13 20:15:35 +0300

    Revert "perf: enable WebP transform for remote service logos"
    This reverts commit 1462d6a8d6f3064e83650348c4b7c747d44912aa.

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -171,9 +171,8 @@ export default defineConfig({
   image: {
     remotePatterns: [
       {
-        protocol: new URL(SITE_URL).protocol.slice(0, -1),
+        protocol: 'https',
         hostname: new URL(SITE_URL).hostname,
-        port: new URL(SITE_URL).port || undefined,
         pathname: '/files/**',
       },
     ],
